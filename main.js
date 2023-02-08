09769382ee25efeeb8252d1665d92ac9262e172d
vishal krishna <121739011+vishalkrishna8@users.noreply.github.com>
2023-02-08 08:04:44 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
 
-/*signUpButton.addEventListener('click',() => 
+signUpButton.addEventListener('click',() => 
 container.classList.add('right-panel-active') 
-);*/
+);
 
 signInButton.addEventListener('click',() => 
 container.classList.remove('right-panel-active')
