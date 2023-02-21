868746d72c866c9926e21f293dc71742675f643a
vishal krishna <121739011+vishalkrishna8@users.noreply.github.com>
2023-02-21 12:56:42 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const signUpButton = document.getElementById('signUp');
-let signInButton = document.getElementById('signIn');
+const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
 
 signUpButton.addEventListener('click',() => 
