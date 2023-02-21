df9e89ea447d45a1702fc33ed10ef22d865cac13
vishal krishna <121739011+vishalkrishna8@users.noreply.github.com>
2023-02-21 12:55:31 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const signUpButton = document.getElementById('signUp');
+let signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
 
